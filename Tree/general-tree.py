a3468b2af30d35017eb769d4f96e2c66d208a82e
ripun <ripunjaytiwari8@gmail.com>
2024-02-02 00:05:08 +0530

    improved tree while loop correction

--- Tree/general-tree.py
+++ Tree/general-tree.py
@@ -1,13 +1,8 @@
 class TreeNode:
-    
     def __init__(self, data):
         self.data = data
         self.children = []
         self.parent = None
-
-    def add_child(self, child):
-        child.parent = self
-        self.children.append(child)
 
     def get_level(self):
         level = 0
@@ -15,49 +10,43 @@ class TreeNode:
         while p:
             level += 1
             p = p.parent
+
         return level
-    
+
     def print_tree(self):
-        spaces = " " * self.get_level() * 4
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|__" if self.parent else ""
         print(spaces + self.data)
         if self.children:
             for child in self.children:
                 child.print_tree()
 
-
-
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
 
 def build_product_tree():
-
     root = TreeNode("Electronics")
-    
+
     laptop = TreeNode("Laptop")
     laptop.add_child(TreeNode("Mac"))
     laptop.add_child(TreeNode("Surface"))
-    laptop.add_child(TreeNode("Thick Pad"))
+    laptop.add_child(TreeNode("Thinkpad"))
 
     cellphone = TreeNode("Cell Phone")
-    cellphone.add_child(TreeNode("IPhone"))
-    cellphone.add_child(TreeNode("Pixal"))
+    cellphone.add_child(TreeNode("iPhone"))
+    cellphone.add_child(TreeNode("Google Pixel"))
     cellphone.add_child(TreeNode("Vivo"))
 
-    tv = TreeNode("Televisions")
+    tv = TreeNode("TV")
+    tv.add_child(TreeNode("Samsung"))
     tv.add_child(TreeNode("LG"))
-    tv.add_child(TreeNode("Sony"))
-    tv.add_child(TreeNode("Samaung"))
-    onida = TreeNode("Onida")
-    tv.add_child(onida)
 
     root.add_child(laptop)
     root.add_child(cellphone)
     root.add_child(tv)
 
-    # print(onida.get_level())
+    root.print_tree()
 
-    return root.print_tree()
-
-
-if __name__ == "__main__":
-    root = build_product_tree()
-  #  print(root.children)
-    
+if __name__ == '__main__':
+    build_product_tree()
